e179f0c5bccd15e318ccfe637342650804a9343e
deborah luckey <dlu6994904@aol.com>
2021-04-02 23:03:32 -0500

    implemented firebae utils

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss'
-import FormInput from '../form-input/form-input.component';
+
 
 const Header = ({ currentUser }) => (
     <div className='header'>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,11 +5,39 @@ import 'firebase/auth';
 const config = {
     apiKey: "AIzaSyC4Ax225MU_PL4jK2Sdfwv-5pol4lGstg4",
     authDomain: "crwn-db-751d5.firebaseapp.com",
+    databaseURL: "https://crwn-db-751d5-default-rtdb.firebaseio.com",
     projectId: "crwn-db-751d5",
     storageBucket: "crwn-db-751d5.appspot.com",
     messagingSenderId: "980943255962",
     appId: "1:980943255962:web:1f858d8d5b7108a3f6e223"
   };
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {    
+        console.log('error creating user', error.message);
+      }
+
+    }
+
+    return userRef;
+};
 
   firebase.initializeApp(config);
 
